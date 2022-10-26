198cff24f1ef07fd46c5a349d55059007d48d461
Galih Rakasiwi <116142298+galer12@users.noreply.github.com>
2022-10-26 13:45:43 +0700

    Add files via upload

---
+++ anggota.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 26, 2022 at 08:43 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pramuka`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `anggota`
+--
+
+CREATE TABLE `anggota` (
+  `id` int(10) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `kelas` varchar(10) NOT NULL,
+  `sangga` varchar(15) NOT NULL,
+  `ambalan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `anggota`
+--
+
+INSERT INTO `anggota` (`id`, `nama`, `kelas`, `sangga`, `ambalan`) VALUES
+(1, 'RD.Galih Rakasiwi', 'XI RPL', 'Pendobrak', 'Otto Iskandardinata'),
+(2, 'Difka Muhamad Awalun Aksan ', 'XII KI A', 'Pendobrak', 'Otto Iskandardinata'),
+(3, 'Abeng', 'XI RPL', 'Pendobrak', 'Otto Iskandardinata'),
+(4, 'Fadhil', 'X ATPH', 'Pendobrak', 'Otto Iskandardinata');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `anggota`
+--
+ALTER TABLE `anggota`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `anggota`
+--
+ALTER TABLE `anggota`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
